80073b24b27aef5f72c2fce4462ea2e4862d80c0
Jovan M <77690201+miljkovicjovan@users.noreply.github.com>
2021-03-15 17:40:25 +0100

    Delete EPISODE 3 directory

--- EPISODE 3/Variables.c
+++
@@ -1,8 +0,0 @@
-// find the sum of two numbers
-int FirstNumber, SecondNumber, SumOfTwoNumbers;
-FirstNumber = 4;
-FirstNumber = 8;
-
-// find the circumference of a circle given a radius
-
-const int PI = 3.14;
