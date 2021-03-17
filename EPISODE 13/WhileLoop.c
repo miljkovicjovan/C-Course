3258ce6f28fa8fbe2b54ae22e91f9d963cecdf25
Jovan M <77690201+miljkovicjovan@users.noreply.github.com>
2021-03-17 22:40:36 +0100

    While Loops

---
+++ EPISODE 13/WhileLoop.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+
+    // making variables
+    int number = 0;
+
+    // while loop
+    while (number < 5) {
+        printf("Number is %d\n", ++number);
+    }
+        
+    return 0;
+}
