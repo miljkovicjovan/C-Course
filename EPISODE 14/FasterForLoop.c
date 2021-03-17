bdacfae55a42b850f39258845752367a9fad4231
Jovan M <77690201+miljkovicjovan@users.noreply.github.com>
2021-03-17 22:56:13 +0100

    For Loop

---
+++ EPISODE 14/FasterForLoop.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+ 
+int main () {
+    
+    // for loop
+    for( int i = 1; i < 6; i++ ){
+        printf("Number is %d\n", i);
+    }
+    
+    return 0;
+}
---
+++ EPISODE 14/ForLoop.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+ 
+int main () {
+    
+    // making the variable
+    int i;
+    
+    // for loop
+    for( i = 1; i < 6; i = i + 1 ){
+      printf("Number is %d\n", i);
+    }
+    
+    return 0;
+}
