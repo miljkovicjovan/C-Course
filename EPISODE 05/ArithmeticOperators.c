efdc336f99bf773120cb38fa8b69d12f6f8f7c37
Jovan M <77690201+miljkovicjovan@users.noreply.github.com>
2021-03-17 20:57:06 +0100

    Arithmetic Operators Increment Decrement

---
+++ EPISODE 05/ArithmeticOperators.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main() {
+    // make two variables with a value of 10 and 20
+    int x = 10, y = 20;
+    
+    // Addition
+    int Addition = x + y;
+    printf("The Addition result is %d\n", Addition);
+
+    // Subtraction
+    int Subtraction = x - y;
+    printf("The Addition result is %d\n", Subtraction);
+
+    // Multiplication
+    int Multiplication = x * y;
+    printf("The Multiplication result is %d\n", Multiplication);
+
+    // Division
+    int Division = y / x;
+    printf("The Division result is %d\n", Division);
+
+    // Modulo
+    int Modulo = y % x;
+    printf("The Modulo result is %d", Modulo);
+
+    return 0;
+}
